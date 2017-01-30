636f8f0491a4a0fa77606ac3f71043f1f53142af
Chris Thomas <faore@onhaven.net>
2017-01-29 22:16:35 -0700

    Compiler now reads file passed in as argument and parses it.

--- LITTLECompiler.java
+++ LITTLECompiler.java
@@ -1,33 +1,21 @@
 import org.antlr.v4.runtime.*;
 import java.util.*;
+import java.io.File;
+import java.util.Scanner;
 
 public class LITTLECompiler {
-	public static void main(String[] args) {
-		System.out.println();
-		ANTLRInputStream input = new ANTLRInputStream("PROGRAM \"test\" fibonacci");
+	public static void main(String[] args) throws Exception {
+		//Read the entire file supplied into the input stream.
+        //This will take the argument from the command line. Run as: java LITTLECompiler inputs/loop.micro
+		ANTLRInputStream input = new ANTLRInputStream(new Scanner(new File(args[0])).useDelimiter("\\Z").next());
 		LITTLELexer lexer = new LITTLELexer(input);
 		Vocabulary vocabulary = lexer.getVocabulary();
 
 		Token token = null;
 		do {
 			token = lexer.nextToken();
-			System.out.println(vocabulary.getSymbolicName(token.getType()));
-			System.out.println(token.getText());
+			System.out.println(vocabulary.getSymbolicName(token.getType()) + ": " + token.getText());
 		} while (token.getType() != Token.EOF);
 
-		// original attempt
-		/*CommonTokenStream tokens = new CommonTokenStream(lexer);
-		List<Token> token_list = tokens.getTokens();
-		
-		System.out.print("\t");
-		System.out.print(token_list.size());
-		System.out.println(" Tokens Found.");
-		
-		for (Token t : token_list) {
-			System.out.println(t.getType());
-			System.out.println(t.getText());
-		}
-		//LITTLEParser parser = new LITTLEParser(tokens);
-		//parser.eval();*/
 	}
 }
