857005a805e4ea88de1f43a3e2240bad1cc044cf
kyle.johnson26 <b19j513@bz-cs-cslab18l.msu.montana.edu>
2017-02-03 13:38:00 -0700

    added catch for FileNotFoundException

--- LITTLECompiler.java
+++ LITTLECompiler.java
@@ -1,7 +1,7 @@
 import org.antlr.v4.runtime.*;
+
 import java.util.*;
-import java.io.File;
-import java.util.Scanner;
+import java.io.*;
 
 public class LITTLECompiler {
 	public static void main(String[] args) throws Exception {
@@ -9,24 +9,33 @@ public class LITTLECompiler {
 			// Read the entire file supplied into the input stream.
 			//	This will take the argument from the command line.
 			//	Run as: java LITTLECompiler inputs/loop.micro
-			ANTLRInputStream input = new ANTLRInputStream(new Scanner(new File(args[0])).useDelimiter("\\Z").next());
-			LITTLELexer lexer = new LITTLELexer(input);
-			Vocabulary vocabulary = lexer.getVocabulary();
+			ANTLRInputStream inputStream = null;
+			try {
+				inputStream = new ANTLRInputStream(new Scanner(new File(args[0])).useDelimiter("\\Z").next());
+			} catch (FileNotFoundException e) {
+				System.out.println("File '" + args[0] + "' Not Found");
+			}
+			
+			if (inputStream != null) {
+				LITTLELexer lexer = new LITTLELexer(inputStream);
+				// get the vocabulary
+				Vocabulary vocabulary = lexer.getVocabulary();
 
-			// get the first token
-			Token token = lexer.nextToken();
-			do {
-				// print out the token information
-				System.out.print("Token Type: ");
-				System.out.println(vocabulary.getSymbolicName(token.getType()));
-				System.out.print("Value: ");
-				System.out.println(token.getText());
+				// get the first token
+				Token token = lexer.nextToken();
+				do {
+					// print out the token information
+					System.out.print("Token Type: ");
+					System.out.println(vocabulary.getSymbolicName(token.getType()));
+					System.out.print("Value: ");
+					System.out.println(token.getText());
 
-				// get the next token
-				token = lexer.nextToken();
-			} while (token.getType() != Token.EOF);
+					// get the next token
+					token = lexer.nextToken();
+				} while (token.getType() != Token.EOF);
+			}
 		} else {
-			System.out.println("No Input Provided.");
+			System.out.println("No Input File Provided.");
 		}
 	}
 }
