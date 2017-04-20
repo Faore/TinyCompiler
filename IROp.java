b7b17bd52b5e7ba2e44401b95d75b9e5ca1a1845
kyle <kyle@fredriksapps.com>
2017-04-20 09:37:42 -0600

    added some enters

--- IROp.java
+++ IROp.java
@@ -2,6 +2,19 @@
  * Created by faore on 4/16/17.
  */
 public enum IROp {
-    ADDI, ADDF, SUBI, SUBF, MULTI, MULTF, DIVI, DIVF, STOREI, STOREF, GT, GE, LT, LE, NE, EQ, JUMP, LABEL, READI,
-    READF, WRITEI, WRITEF, WRITES, LINK, RET
+    ADDI, ADDF,
+    SUBI, SUBF,
+    MULTI, MULTF,
+    DIVI, DIVF,
+    STOREI, STOREF,
+    GT, GE,
+    LT, LE,
+    NE, EQ,
+    JUMP,
+    LABEL,
+    READI, READF,
+    WRITEI, WRITEF,
+    WRITES,
+    LINK,
+    RET
 }
