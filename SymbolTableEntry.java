40c67be37454ad04d68070c289087e0c378cb859
kyle <kyle@fredriksapps.com>
2017-04-21 14:15:23 -0600

    added constants

--- SymbolTableEntry.java
+++ SymbolTableEntry.java
@@ -1,4 +1,8 @@
 public class SymbolTableEntry {
+	public static final String TYPE_STRING = "STRING";
+	public static final String TYPE_INT = "INT";
+	public static final String TYPE_FLOAT = "FLOAT";
+
 	public static final String NULL = "_NULL_";
 
 	protected String type;
